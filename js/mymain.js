976d7654d75750d34c6b7d920b5c24f16a9a3e01
Claudia Perez Brito <claudiavperezb@gmail.com>
2018-11-09 21:23:45 -0500

    removedcss

--- js/mymain.js
+++ js/mymain.js
@@ -2,25 +2,22 @@
 
 $(document).ready(() => {
 
-  var flip = 0;
-  $( "#suspectsButton" ).click(function() {
-    $( "#suspects" ).toggle( flip++ % 2 === 0 );
+  $('#suspectsButton').on('click',() => {
+    $('#suspects').fadeToggle(1000);
   });
 
-var flip = 0;
-$( "#battyButton" ).click(function() {
-  $( "#batty" ).toggle( flip++ % 2 === 0 );
-});
+  $('#battyButton').on('click',() => {
+    $('#batty').fadeToggle(1000);
+  });
 
-var flip = 0;
-$( "#laundryButton" ).click(function() {
-  $( "#laundry" ).toggle( flip++ % 2 === 0 );
-});
+  $('#laundryButton').on('click',() => {
+    $('#laundry').fadeToggle(1000);
+  });
 
-var flip = 0;
-$( "#birthdayButton" ).click(function() {
-  $( "#birthday" ).toggle( flip++ % 2 === 0 );
-});
+  $('#birthdayButton').on('click',() => {
+    $('#birthday').fadeToggle(1000);
+  });
+
 
 //this bracket thing & parenthesis MUST STAY AT THE END//
 })
